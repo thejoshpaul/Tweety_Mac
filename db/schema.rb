e9a0117f10f040a9b92befb413b1bdf855f38167
thejoshpaul <thejoshpaul@yahoo.com>
2018-03-22 14:54:04 -0500

    generated tweet model and set user tweets associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180322193247) do
+ActiveRecord::Schema.define(version: 20180322195026) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.string "message"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
